86c726db0b2b9b574fddcba0c33e87f4498bdf95
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-12-04 00:24:59 +0100

    Add playername fields

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,9 +1,9 @@
 public class TennisGame1 implements TennisGame {
 
+    private final String player1Name;
+    private final String player2Name;
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -11,10 +11,11 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1")) {
             m_score1 += 1;
-        else
+        } else {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
@@ -55,13 +56,16 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getAdvantageOrWin() {
-        String score;
         int minusResult = m_score1 - m_score2;
-        if (minusResult == 1) score = "Advantage player1";
-        else if (minusResult == -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
-        return score;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
     }
 
     private String getEqualScore() {
