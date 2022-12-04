eb3433cf53ea1da205106a1dd6743dc7fe68ee0a
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-12-04 01:18:05 +0100

    Use player class

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -2,8 +2,6 @@ public class TennisGame1 implements TennisGame {
 
     private final Player p1;
     private final Player p2;
-    private int m_score1 = 0;
-    private int m_score2 = 0;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.p1 = new Player(player1Name);
@@ -12,16 +10,16 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) throws Exception {
         if (playerName.equals(this.p1.getName())) {
-            m_score1 += 1;
+            this.p1.addPoint();
         } else if (playerName.equals(this.p2.getName())) {
-            m_score2 += 1;
+            this.p2.addPoint();
         } else {
             throw new Exception("%s is not a valid playerName.".formatted(playerName));
         }
     }
 
     public String getScore() throws Exception {
-        if (scoreIsEqual(m_score1, m_score2)) {
+        if (scoreIsEqual(this.p1.getScore(), this.p2.getScore())) {
             return getEqualScore();
         }
         if (isAdvantageOrWin()) {
@@ -31,7 +29,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     private boolean isAdvantageOrWin() {
-        return m_score1 >= 4 || m_score2 >= 4;
+        return this.p1.getScore() >= 4 || this.p2.getScore() >= 4;
     }
 
     private boolean scoreIsEqual(int m_score1, int m_score2) {
@@ -39,8 +37,8 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getNormalScore() throws Exception {
-        String p1 = playerScore(m_score1);
-        String p2 = playerScore(m_score2);
+        String p1 = playerScore(this.p1.getScore());
+        String p2 = playerScore(this.p2.getScore());
         return "%s-%s".formatted(p1, p2);
     }
 
@@ -55,7 +53,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getAdvantageOrWin() {
-        int minusResult = m_score1 - m_score2;
+        int minusResult = this.p1.getScore() - this.p2.getScore();
         if (minusResult == 1) {
             return "Advantage %s".formatted(this.p1.getName());
         } else if (minusResult == -1) {
@@ -68,7 +66,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getEqualScore() {
-        return switch (m_score1) {
+        return switch (this.p1.getScore()) {
             case 0 -> "Love-All";
             case 1 -> "Fifteen-All";
             case 2 -> "Thirty-All";
